8f64c5ade68bd94d47e8e292318e0bf264d4cecf
erichensleyibm <eric.hensley@ibm.com>
2018-06-19 17:14:07 -0400

    python2_3

--- welcome.py
+++ welcome.py
@@ -214,7 +214,8 @@ def pred_time():
         event = ['"None"' if str(i) == '\t\xa0' else '"'+str(i)+'"' for i in event]
     except:
         event = ['"None"' if i.encode('ascii', 'ignore').decode('ascii') == '' else i for i in event]
-        
+        event = ['"None"' if i.encode('ascii', 'ignore').decode('ascii') == '\t\xa0' else i for i in event]
+
     cond = tree.xpath('//tr[@class="no-metars"]/td[%i]/text()' % (headers.index('conditions')))
     cond = ['"'+str(i)+'"' for i in cond]
         
