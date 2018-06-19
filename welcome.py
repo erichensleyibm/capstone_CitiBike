3b3f9b196c717affece56ab2f48b54f011112bd5
erichensleyibm <eric.hensley@ibm.com>
2018-06-19 11:39:41 -0400

    bluemix_test_2

--- welcome.py
+++ welcome.py
@@ -12,6 +12,11 @@
 
 import json
 import os
+
+port = os.getenv('PORT', '5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(port))
+    
 import requests
 import datetime
 from flask import Flask, render_template, request
